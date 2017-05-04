fd299fb5547ce5e297733e056115469c21169529
ilkeryasinaksoy <ilkeryasinaksoy@gmail.com>
2017-05-04 21:52:46 +0300

    Kodlar eklendi.

---
+++ WindowsFormsApplication6/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace WindowsFormsApplication6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.panel11 = new System.Windows.Forms.Panel();
+            this.panel12 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel13 = new System.Windows.Forms.Panel();
+            this.panel15 = new System.Windows.Forms.Panel();
+            this.panel16 = new System.Windows.Forms.Panel();
+            this.panel17 = new System.Windows.Forms.Panel();
+            this.panel18 = new System.Windows.Forms.Panel();
+            this.panel19 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(0, 20);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(20, 218);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Location = new System.Drawing.Point(0, 60);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(780, 10);
+            this.panel2.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Black;
+            this.panel3.Location = new System.Drawing.Point(60, 20);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(10, 218);
+            this.panel3.TabIndex = 4;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Black;
+            this.panel5.Location = new System.Drawing.Point(110, 20);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(10, 218);
+            this.panel5.TabIndex = 6;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.Black;
+            this.panel7.Location = new System.Drawing.Point(160, 20);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(10, 218);
+            this.panel7.TabIndex = 7;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.Black;
+            this.panel8.Location = new System.Drawing.Point(210, 20);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(10, 218);
+            this.panel8.TabIndex = 8;
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.Color.Black;
+            this.panel9.Location = new System.Drawing.Point(260, 20);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(10, 218);
+            this.panel9.TabIndex = 9;
+            // 
+            // panel10
+            // 
+            this.panel10.BackColor = System.Drawing.Color.Black;
+            this.panel10.Location = new System.Drawing.Point(360, 20);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(10, 218);
+            this.panel10.TabIndex = 10;
+            // 
+            // panel11
+            // 
+            this.panel11.BackColor = System.Drawing.Color.Black;
+            this.panel11.Location = new System.Drawing.Point(310, 20);
+            this.panel11.Name = "panel11";
+            this.panel11.Size = new System.Drawing.Size(10, 218);
+            this.panel11.TabIndex = 11;
+            // 
+            // panel12
+            // 
+            this.panel12.BackColor = System.Drawing.Color.Black;
+            this.panel12.Location = new System.Drawing.Point(760, 20);
+            this.panel12.Name = "panel12";
+            this.panel12.Size = new System.Drawing.Size(20, 218);
+            this.panel12.TabIndex = 12;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.Black;
+            this.panel6.Location = new System.Drawing.Point(410, 20);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(10, 218);
+            this.panel6.TabIndex = 22;
+            // 
+            // panel13
+            // 
+            this.panel13.BackColor = System.Drawing.Color.Black;
+            this.panel13.Location = new System.Drawing.Point(460, 20);
+            this.panel13.Name = "panel13";
+            this.panel13.Size = new System.Drawing.Size(10, 218);
+            this.panel13.TabIndex = 23;
+            // 
+            // panel15
+            // 
+            this.panel15.BackColor = System.Drawing.Color.Black;
+            this.panel15.Location = new System.Drawing.Point(510, 20);
+            this.panel15.Name = "panel15";
+            this.panel15.Size = new System.Drawing.Size(10, 218);
+            this.panel15.TabIndex = 24;
+            // 
+            // panel16
+            // 
+            this.panel16.BackColor = System.Drawing.Color.Black;
+            this.panel16.Location = new System.Drawing.Point(560, 20);
+            this.panel16.Name = "panel16";
+            this.panel16.Size = new System.Drawing.Size(10, 218);
+            this.panel16.TabIndex = 25;
+            // 
+            // panel17
+            // 
+            this.panel17.BackColor = System.Drawing.Color.Black;
+            this.panel17.Location = new System.Drawing.Point(610, 20);
+            this.panel17.Name = "panel17";
+            this.panel17.Size = new System.Drawing.Size(10, 218);
+            this.panel17.TabIndex = 26;
+            // 
+            // panel18
+            // 
+            this.panel18.BackColor = System.Drawing.Color.Black;
+            this.panel18.Location = new System.Drawing.Point(660, 20);
+            this.panel18.Name = "panel18";
+            this.panel18.Size = new System.Drawing.Size(10, 218);
+            this.panel18.TabIndex = 27;
+            // 
+            // panel19
+            // 
+            this.panel19.BackColor = System.Drawing.Color.Black;
+            this.panel19.Location = new System.Drawing.Point(710, 20);
+            this.panel19.Name = "panel19";
+            this.panel19.Size = new System.Drawing.Size(10, 218);
+            this.panel19.TabIndex = 28;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Black;
+            this.panel4.Location = new System.Drawing.Point(0, 235);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(780, 20);
+            this.panel4.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGreen;
+            this.ClientSize = new System.Drawing.Size(780, 254);
+            this.Controls.Add(this.panel19);
+            this.Controls.Add(this.panel18);
+            this.Controls.Add(this.panel17);
+            this.Controls.Add(this.panel16);
+            this.Controls.Add(this.panel15);
+            this.Controls.Add(this.panel13);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel12);
+            this.Controls.Add(this.panel10);
+            this.Controls.Add(this.panel11);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Abaküs Simülasyonu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Panel panel11;
+        private System.Windows.Forms.Panel panel12;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel13;
+        private System.Windows.Forms.Panel panel15;
+        private System.Windows.Forms.Panel panel16;
+        private System.Windows.Forms.Panel panel17;
+        private System.Windows.Forms.Panel panel18;
+        private System.Windows.Forms.Panel panel19;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
+
---
+++ WindowsFormsApplication6/Form1.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Media;
+
+
+namespace WindowsFormsApplication6
+{
+
+
+
+
+
+public partial class Form1 : Form
+
+    {
+
+
+
+
+        public class beslik
+        {
+            
+            static int adet = 0;
+            static int sayi;
+            int nesneNo;
+            static  Panel labelPaneli = new Panel();
+            static Label[] toplam = new Label[14];
+            Panel ust = new Panel();
+            bool durum = false;
+            SoundPlayer ses = new SoundPlayer(Application.StartupPath+"\\click.wav");
+            
+            public beslik(Form frm,int gelen)
+            {
+                if (adet == 0)
+                {
+                    labelPaneli.Size = new Size(780, 20);
+                    labelPaneli.Location = new Point(0, 0);
+                    labelPaneli.BackColor = Color.Black;
+                    frm.Controls.Add(labelPaneli);
+
+                    for (int i = 0; i < 14; i++)
+                    {
+                        toplam[i] = new Label();
+                        toplam[i].Size = new Size(16, 16);
+                        toplam[i].Location = new Point(58 + i * 50, 2);
+                        toplam[i].BackColor = Color.Transparent;
+                        toplam[i].ForeColor = Color.White;
+                        toplam[i].Font = new Font("Microsoft Sans Serif", 9.5f, FontStyle.Bold);
+                        toplam[i].BringToFront();
+
+                        for (int j = 0; j < 14; j++)
+                            toplam[i].Text = "0";
+
+                    }
+
+                    for (int i = 0; i < 14; i++)
+                        labelPaneli.Controls.Add(toplam[i]);
+
+                    
+
+                }
+                sayi = gelen;
+                ust.Size = new Size(40, 20);
+                ust.Location = new Point(45+adet*50, 20);
+                ust.BackColor = Color.Red;
+                ust.Click += new EventHandler(ust_Click);
+                frm.Controls.Add(ust);
+                ust.BringToFront();
+                nesneNo = adet;
+                adet++;
+                
+            }
+
+
+            public static void toplamArttir(char artis,int hangiKolon)
+            {
+                if (artis == '1')
+                    toplam[hangiKolon].Text = (Convert.ToInt32(toplam[hangiKolon].Text) + 1).ToString();
+                if (artis == '5')
+                    toplam[hangiKolon].Text = (Convert.ToInt32(toplam[hangiKolon].Text) + 5).ToString();
+
+            }
+
+            public static void toplamAzalt(char azalis, int hangiKolon)
+            {
+                if (azalis == '1')
+                    toplam[hangiKolon].Text = (Convert.ToInt32(toplam[hangiKolon].Text) - 1).ToString();
+                if (azalis == '5')
+                    toplam[hangiKolon].Text = (Convert.ToInt32(toplam[hangiKolon].Text) - 5).ToString();
+
+            }
+
+
+
+            public void ust_Click(object sender,EventArgs e)
+            {
+
+                if (durum == false)
+                {
+                    ust.Top = ust.Top + 20;
+                    ust.BackColor = Color.Yellow;
+                    ses.Play();
+                    toplamArttir('5', nesneNo);
+                    durum = true;
+                }
+                else
+                {
+                    ust.Top = ust.Top - 20;
+                    ust.BackColor = Color.Red;
+                    ses.Play();
+                    toplamAzalt('5', nesneNo);
+                    durum = false;
+
+                }
+
+            }
+
+
+
+        }
+
+        public class birlik
+        {
+            static int[] dizi = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            static int sayi = 0;
+            int nesneNo;
+            int kolon;
+            SoundPlayer ses = new SoundPlayer(Application.StartupPath+@"\\click.wav");
+            Panel ust = new Panel();
+            bool durum = false;
+            
+
+            public birlik(Form frm, int gelen)
+            {
+                
+                kolon = gelen;
+                ust.Size = new Size(40, 20);
+                ust.Location = new Point(45+kolon*50,90+sayi*25);
+                ust.BackColor = Color.Red;
+                ust.Click += new EventHandler(ust_Click);
+
+                frm.Controls.Add(ust);
+
+                ust.BringToFront();
+                nesneNo = sayi;
+                sayi++;
+                if (sayi % 4 == 0)
+                    sayi = 0;
+               
+
+            }
+
+            public void oynat()
+            {
+
+                //if (durum==false)
+                //{
+                //    ust.Top = ust.Top - 20;
+                //    ust.BackColor = Color.Yellow;
+                //    durum = true;
+                //}
+                //else
+                //{
+                //    ust.Top = ust.Top + 20;
+                //    ust.BackColor = Color.Red;
+                //    durum = false;
+
+                //}
+                
+
+                switch (nesneNo)
+                {
+                    case 0:
+                        if (durum == false)
+                        {
+                            ust.Top = ust.Top - 20;
+                            ust.BackColor = Color.Yellow;
+                            ses.Play();
+                            beslik.toplamArttir('1', kolon);
+                            dizi[nesneNo] = 1;
+                            durum = true;
+                        }
+                        else if(durum==true && dizi[nesneNo+1]==0)
+                        {
+                            ust.Top = ust.Top + 20;
+                            ust.BackColor = Color.Red;
+                            ses.Play();
+                            beslik.toplamAzalt('1', kolon);
+                            dizi[nesneNo] = 0;
+                            durum = false;
+
+                        }
+                        break;
+
+                    case 1:
+                        if (durum == false && dizi[nesneNo-1]==1)
+                        {
+                            ust.Top = ust.Top - 20;
+                            ust.BackColor = Color.Yellow;
+                            ses.Play();
+                            beslik.toplamArttir('1', kolon);
+                            dizi[nesneNo] = 1;
+                            durum = true;
+                        }
+                        else if (durum == true && dizi[nesneNo+1] == 0)
+                        {
+                            ust.Top = ust.Top + 20;
+                            ust.BackColor = Color.Red;
+                            ses.Play();
+                            beslik.toplamAzalt('1', kolon);
+                            dizi[nesneNo] = 0;
+                            durum = false;
+
+                        }
+                        break;
+
+                    case 2:
+                        if (durum == false && dizi[nesneNo-1] == 1)
+                        {
+                            ust.Top = ust.Top - 20;
+                            ust.BackColor = Color.Yellow;
+                            ses.Play();
+                            beslik.toplamArttir('1', kolon);
+                            dizi[nesneNo] = 1;
+                            durum = true;
+                        }
+                        else if (durum == true && dizi[nesneNo + 1] == 0)
+                        {
+                            ust.Top = ust.Top + 20;
+                            ust.BackColor = Color.Red;
+                            ses.Play();
+                            beslik.toplamAzalt('1', kolon);
+                            dizi[nesneNo] = 0;
+                            durum = false;
+
+                        }
+                        break;
+
+                    case 3:
+                        if (durum == false && dizi[nesneNo-1] == 1)
+                        {
+                            ust.Top = ust.Top - 20;
+                            ust.BackColor = Color.Yellow;
+                            ses.Play();
+                            beslik.toplamArttir('1', kolon);
+                            dizi[nesneNo] = 1;
+                            durum = true;
+                        }
+                        else if (durum == true && dizi[nesneNo + 1] == 0)
+                        {
+                            ust.Top = ust.Top + 20;
+                            ust.BackColor = Color.Red;
+                            ses.Play();
+                            beslik.toplamArttir('1', kolon);
+                            dizi[nesneNo] = 0;
+                            durum = false;
+
+                        }
+                        break;
+                       
+
+
+
+
+
+
+                }
+            }
+
+            public void ust_Click(object sender, EventArgs e)
+            {
+
+                oynat();
+
+            }
+
+
+
+        }
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            beslik[] deneme = new beslik[14];
+            birlik[,] deneme2 = new birlik[4,14];   
+
+
+            for (int i = 0; i < 14; i++)
+                deneme[i] = new beslik(this, i);
+
+            for (int i = 0; i < 14; i++)
+                for (int j = 0; j < 4; j++)
+                    deneme2[j, i] = new birlik(this, i);
+
+            //Label[] lbl = new Label[14];
+
+            //for(int i = 0;i < 14; i++)
+            //{
+            //    lbl[i] = new Label();
+            //    lbl[i].Size = new Size(16, 16);
+            //    lbl[i].Location = new Point(58 + i*50, 2);
+            //    lbl[i].BackColor = Color.Transparent;
+            //    lbl[i].ForeColor = Color.White;
+            //    lbl[i].Font = new Font("Microsoft Sans Serif", 9.5f, FontStyle.Bold);
+            //    lbl[i].BringToFront();
+
+            //}
+
+            //for (int i = 0; i < 14; i++)
+            //    panel14.Controls.Add(lbl[i]);
+
+            //lbl[4].Text = "123";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+           
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Label deneme = new Label();
+            deneme.Location = new Point(58, 2);
+            deneme.BringToFront();
+            this.Controls.Add(deneme);
+            deneme.Text = "2324";
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            this.Dispose();
+            Form1 frm = new Form1();
+            frm.Show();
+        }
+    }
+}
